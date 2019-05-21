3cd129a86d2a5c23fc9810daded17f302fa72074
luanvieirazup <luan.vieira@zup.com.br>
2019-05-21 14:50:23 -0300

    Accordion

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
-const tabMenu = document.querySelectorAll('.js-tabMenu li');
-const tabContent = document.querySelectorAll('.js-tabContent section');
-
-tabContent[0].classList.add('ativo');
-
-if (tabMenu.length && tabContent.length) {
-    function activeTab(index) {
-        tabContent.forEach((section) => {
-            section.classList.remove('ativo');
-        });
-        tabContent[index].classList.add('ativo');
-    }
+function initTabNav () {
+    const tabMenu = document.querySelectorAll('.js-tabMenu li');
+    const tabContent = document.querySelectorAll('.js-tabContent section');
+    const activeClass = 'ativo';
+      
+    tabContent[0].classList.add(activeClass);
     
-    tabMenu.forEach((itemMenu, index) => {
-        itemMenu.addEventListener('click', () => {
-            activeTab(index);
+    if (tabMenu.length && tabContent.length) {
+        function activeTab(index) {
+            tabContent.forEach((section) => {
+                section.classList.remove(activeClass);
+            });
+            tabContent[index].classList.add(activeClass);
+        }
+        
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                activeTab(index);
+            });
         });
-    });
+    }
 }
+initTabNav();
+
+function initAccordion () {    
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    const activeClass = 'ativo';
+    if(accordionList.length) {
+        accordionList[0].classList.add(activeClass);
+        accordionList[0].nextElementSibling.classList.add(activeClass)
+        
+        function activeAccordion() {
+            this.classList.toggle(activeClass);
+            this.nextElementSibling.classList.toggle(activeClass);
+            
+        }
+        
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAccordion);
+        });
+    }
+}
+initAccordion();
