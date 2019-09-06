bc0c299322169dd4c748a4538e2f51f295a5bb31
luan <luan.vieira@zup.com.br>
2019-09-06 15:57:23 -0300

    Finish tooltip

--- modules/tooltip.js
+++ modules/tooltip.js
@@ -10,18 +10,25 @@ tooltips.forEach((item) => {
 
 function onMouseOver(event) {
     const tooltipBox = criarTooltipBox(this);
-    tooltipBox.style.top = event.pageY + 'px';
-    tooltipBox.style.left = event.pageX + 'px';
-
+    onMouseLeave.element = this;
     onMouseLeave.tooltipBox = tooltipBox;
     this.addEventListener('mouseleave', onMouseLeave);
-
+    onMouseMove.tooltipBox = tooltipBox;
+    this.addEventListener('mousemove', onMouseMove)
 };
 
+const onMouseMove = {
+    handleEvent(event) {
+        this.tooltipBox.style.top = event.pageY + 20 + 'px';
+        this.tooltipBox.style.left = event.pageX + 20 + 'px';
+    }
+}
+
 const onMouseLeave = {
-    tooltipBox: '',
     handleEvent() {
         this.tooltipBox.remove();
+        this.element.removeEventListener('mouseleave', onMouseLeave);
+        this.element.removeEventListener('mousemove', onMouseMove);
     }
 }
 
